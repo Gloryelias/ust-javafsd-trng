cd75e488c5ea038b25079ec056890e34f502e668
Gloryelias <112387748+Gloryelias@users.noreply.github.com>
2022-09-03 11:23:20 +0530

    Add files via upload

---
+++ JAVA/SDLC/DUMMY/Array_1.java
@@ -0,0 +1,22 @@
+package com.ust;
+
+public class Array_1 {
+	public static void main(String args[])
+	 {
+		String str[]=new String[5];
+		//str= {"java","c","c++",}
+		str[0]="java";
+		str[1]="c";
+		str[2]="c++";
+		System.out.println(str[2]);
+				
+		int[] arr=new int[3];
+		char[] c= {'a','e','i','o','u'};
+		//cc[5]);
+		
+		int arr1[][]= {{1,2},{3,4}};
+		System.out.println(arr1[1][1]);
+	 }
+	
+
+}
