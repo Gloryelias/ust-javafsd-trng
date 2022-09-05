b79e2b58310d993aaa36ffe28397e80ae65313d5
Gloryelias <112387748+Gloryelias@users.noreply.github.com>
2022-09-05 10:04:38 +0530

    Add files via upload

---
+++ JAVA/SDLC/DUMMY/Paramertized_constructors.java
@@ -0,0 +1,26 @@
+package com;
+class this_Test {
+	int val1;
+	int val2;
+	
+	//Parameterized constructors
+	this_Test(int val1,int val2)
+	{
+		this.val1=val1+val2;
+		this.val2=val2+val2;
+	}
+	
+	void display()
+	{
+		System.out.println("Value val1=" + val1 + "Value val2="+ val2);
+	}
+	
+}
+class Paramertized_constructors{
+	public static void main(String[] args)
+	{
+		this_Test object=new this_Test(5,10);
+		object.display();
+		
+	}
+}
